aa085d7d25c31b65f510915a8d8b13d4d114d9cd
Whitney Hiemstra <whitcolorado@gmail.com>
2015-07-21 13:08:03 -0600

    Logout feature implemented

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   def create
-    # render text: request.env["omniauth.auth"].inspect
     if user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = user.id
       redirect_to dashboard_path
