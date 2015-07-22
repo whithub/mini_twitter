1bf373455ced4818637076a368f4b34b634d9347
Whitney Hiemstra <whitcolorado@gmail.com>
2015-07-21 23:17:13 -0600

    Able to display twitter client info inside tweet. WIP-need to capture the original tweets owner for retweets...

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,21 +19,6 @@ class User < ActiveRecord::Base
 
     user
 
-    # if user = find_by(uid: auth_info.extra.raw_info.user_id)
-    #   user
-    # else
-    #   create({name: auth_info.extra.raw_info.name,
-    #           screen_name: auth_info.extra.raw_info.screen_name,
-    #           location: auth_info.extra.raw_info.location,
-    #           statuses_count: auth_info.extra.raw_info.statuses_count,
-    #           friends_count: auth_info.extra.raw_info.friends_count,
-    #           followers_count: auth_info.extra.raw_info.followers_count,
-    #           uid: auth_info.extra.raw_info.user_id,
-    #           provider: auth_info.provider,
-    #           oauth_token: auth_info.credentials.token,
-    #           oauth_token_secret: auth_info.credentials.secret,
-    #     })
-    # end
   end
 
   def twitter_client
@@ -45,11 +30,11 @@ class User < ActiveRecord::Base
     end
   end
 
-  def twitter_name
-    twitter_client.name
-  end
+  # def current_profile
+  #   twitter_client.user
+  # end
 
-  def twitter_screen_name
-    twitter_client.screen_name
+  def twitter_timeline
+    twitter_client.home_timeline
   end
 end
