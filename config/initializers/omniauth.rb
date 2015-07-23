5fd132e6a6b06934be9150133cf7e5682d299cd3
Whitney Hiemstra <whitcolorado@gmail.com>
2015-07-22 21:30:55 -0600

    change environment variables to uppercase

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['twitter_key'], ENV['twitter_secret']
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_KEY_SECRET']
 end
