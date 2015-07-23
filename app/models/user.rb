c0780d65b31d219fb95d2d2be790d7a78f8cc396
Whitney Hiemstra <whitcolorado@gmail.com>
2015-07-22 21:42:54 -0600

    bad var names

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,8 +25,8 @@ class User < ActiveRecord::Base
 
   def twitter_client
     @client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV["twitter_key"]
-      config.consumer_secret = ENV["twitter_secret"]
+      config.consumer_key = ENV["TWITTER_KEY"]
+      config.consumer_secret = ENV["TWITTER_KEY_SECRET"]
       config.access_token = self.oauth_token
       config.access_token_secret = self.oauth_token_secret
     end
